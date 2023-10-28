6a1c3595d373096ed33481bc5bb4beb4a7c722e2
Евгений Сапрунов <sapra10062018@gmail.com>
2023-10-28 14:40:01 +0300

    рефактор: поправлен параметер метода

--- src/main/java/by/sapra/module2homework/events/StudentEventPublisher.java
+++ src/main/java/by/sapra/module2homework/events/StudentEventPublisher.java
@@ -1,9 +1,7 @@
 package by.sapra.module2homework.events;
 
-import by.sapra.module2homework.servoces.StudentServices;
-
 public interface StudentEventPublisher {
     void deleteEventPublish(Object source, DeleteEvent any);
 
-    void createStudentPublish(StudentServices eq, CreateEvent any);
+    void createStudentPublish(Object source, CreateEvent any);
 }
